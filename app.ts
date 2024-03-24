0af4f4eae68858da32b934f857331725b7ca63e0
supremood <lappay.john@gmail.com>
2024-03-24 09:20:56 +0800

    update

--- app.ts
+++ app.ts
@@ -1,15 +1,25 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-
+import mongoose, { Schema } from "mongoose";
 dotenv.config();
+
+const database = mongoose.connect(process.env.MONGODB_URI!)
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+  database.then(() => {
+    res.send("Connected");
+
+  }).catch((error) => {
+    res.send(" Not Connected");
+  });
 });
 
+
 app.listen(port, () => {
+
+
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
